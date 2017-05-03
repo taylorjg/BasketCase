4d4a28547ed58c0ce7b4d74e4d698c8f84209f20
Jon Taylor <jont@cakesolutions.net>
2017-05-03 10:01:40 +0100

    Tweak to results count and a bit more work on facets

--- client/js/app.module.js
+++ client/js/app.module.js
@@ -1,6 +1,3 @@
 import { module } from 'angular';
 const app = module('appBasketCase', []);
-// app.run(['$timeout', 'SearchService', function($timeout, SearchService) {
-//     $timeout(() => SearchService.facets());
-// }]);
 export default app;
