85910add111dee70fdd17376328995c5fe6b4f77
Jon Taylor <jont@cakesolutions.net>
2017-05-10 12:19:20 +0100

    Put some structure in place for back end tests

---
+++ server/tests/es2myTests.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+describe('es2my tests', () => {
+
+    describe('#elasticsearchResponseToMyResponse', () => {
+
+        it('test 1', () => {
+            expect([]).to.be.empty;
+        });
+    });
+});
---
+++ server/tests/my2esTests.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+
+describe('my2es tests', () => {
+
+    describe('#myFiltersToElasticsearchFilters', () => {
+
+        it('test 1', () => {
+            expect([]).to.be.empty;
+        });
+    });
+
+    describe('#mySortByToElasticsearchSort', () => {
+
+        it('test 1', () => {
+            expect([]).to.be.empty;
+        });
+    });
+
+    describe('#addAggregationsToRequest', () => {
+
+        it('test 1', () => {
+            expect([]).to.be.empty;
+        });
+    });
+});
