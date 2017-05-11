a8405bc7a1fe4eb62ed05eaf786786affdc1a3b5
Jon Taylor <jont@cakesolutions.net>
2017-05-11 15:26:10 +0100

    Completed facet.components.tests

--- client/tests/facet.component.tests.js
+++ client/tests/facet.component.tests.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 
 const angular = window.angular;
 
-const facet = {
+const termsFacet = {
     facetId: 2,
     displayName: 'Brand',
     isRange: false,
@@ -31,7 +31,41 @@ const facet = {
     ]
 };
 
-const cloneFacet = () => JSON.parse(JSON.stringify(facet));
+const rangeFacet = {
+    facetId: 4,
+    displayName: 'Price',
+    isRange: true,
+    facetValues: [
+        {
+            index: 0,
+            displayName: '&pound;200 or less',
+            key: '*-200.0',
+            to: 200,
+            count: 2,
+            selected: false
+        },
+        {
+            index: 1,
+            displayName: '&pound;200 - &pound;250',
+            key: '200.0-250.0',
+            from: 200,
+            to: 250,
+            count: 7,
+            selected: false
+        },
+        {
+            index: 2,
+            displayName: '&pound;250 or more',
+            key: '250.0-*',
+            from: 250,
+            count: 5,
+            selected: false
+        }
+    ]
+};
+
+const cloneTermsFacet = () => JSON.parse(JSON.stringify(termsFacet));
+const cloneRangeFacet = () => JSON.parse(JSON.stringify(rangeFacet));
 
 describe('facet.component', () => {
 
@@ -50,7 +84,7 @@ describe('facet.component', () => {
     });
 
     it('onReset() deselects all facet values', () => {
-        const clonedFacet = cloneFacet();
+        const clonedFacet = cloneTermsFacet();
         const bindings = {
             facet: clonedFacet,
             onFacetSelectionChanged: angular.noop
@@ -64,7 +98,7 @@ describe('facet.component', () => {
     });
 
     it('onReset() invokes onFacetSelectionChanged', () => {
-        const clonedFacet = cloneFacet();
+        const clonedFacet = cloneTermsFacet();
         let invoked = false;
         const bindings = {
             facet: clonedFacet,
@@ -78,7 +112,7 @@ describe('facet.component', () => {
     });
 
     it('onChange() invokes onFacetSelectionChanged', () => {
-        const clonedFacet = cloneFacet();
+        const clonedFacet = cloneTermsFacet();
         let invoked = false;
         const bindings = {
             facet: clonedFacet,
@@ -91,12 +125,46 @@ describe('facet.component', () => {
         expect(invoked).to.equal(true);
     });
 
-    // TODO:
-    // onChange(value) maintains selection state of all other values when isRange is false
-    // onChange(value) deselects all other values when isRange is true
+    it('onChange(value) maintains selection state of all other values for a terms facet', () => {
+        const clonedFacet = cloneTermsFacet();
+        const bindings = {
+            facet: clonedFacet,
+            onFacetSelectionChanged: angular.noop
+        };
+        clonedFacet.facetValues[0].selected = false;
+        clonedFacet.facetValues[1].selected = true;
+        clonedFacet.facetValues[2].selected = false;
+
+        const controller = $componentController('facet', {}, bindings);
+        clonedFacet.facetValues[0].selected = true;
+        controller.onChange(clonedFacet.facetValues[0]);
+
+        expect(clonedFacet.facetValues[0].selected).to.equal(true);
+        expect(clonedFacet.facetValues[1].selected).to.equal(true);
+        expect(clonedFacet.facetValues[2].selected).to.equal(false);
+    });
+
+    it('onChange(value) deselects all other values for a range facet', () => {
+        const clonedFacet = cloneRangeFacet();
+        const bindings = {
+            facet: clonedFacet,
+            onFacetSelectionChanged: angular.noop
+        };
+        clonedFacet.facetValues[0].selected = false;
+        clonedFacet.facetValues[1].selected = true;
+        clonedFacet.facetValues[2].selected = false;
+
+        const controller = $componentController('facet', {}, bindings);
+        clonedFacet.facetValues[0].selected = true;
+        controller.onChange(clonedFacet.facetValues[0]);
+
+        expect(clonedFacet.facetValues[0].selected).to.equal(true);
+        expect(clonedFacet.facetValues[1].selected).to.equal(false);
+        expect(clonedFacet.facetValues[2].selected).to.equal(false);
+    });
 
     it('anythingSelected() returns false when no values are selected', () => {
-        const clonedFacet = cloneFacet();
+        const clonedFacet = cloneTermsFacet();
         const bindings = {
             facet: clonedFacet,
             onFacetSelectionChanged: angular.noop
@@ -109,7 +177,7 @@ describe('facet.component', () => {
     });
 
     it('anythingSelected() returns true when one value is selected', () => {
-        const clonedFacet = cloneFacet();
+        const clonedFacet = cloneTermsFacet();
         const bindings = {
             facet: clonedFacet,
             onFacetSelectionChanged: angular.noop
@@ -123,7 +191,7 @@ describe('facet.component', () => {
     });
 
     it('anythingSelected() returns true when all values are selected', () => {
-        const clonedFacet = cloneFacet();
+        const clonedFacet = cloneTermsFacet();
         const bindings = {
             facet: clonedFacet,
             onFacetSelectionChanged: angular.noop
