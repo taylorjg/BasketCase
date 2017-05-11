048d9126de66fb8b7134a0dda98348da65a3c5f9
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-05-11 19:31:44 +0100

    allTests => setup

--- client/tests/allTests.js
+++
@@ -1,4 +0,0 @@
-import '../js/app';
-import 'angular-mocks';
-import './facet.component.tests';
-import './facetValue.component.tests';
--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,10 @@ module.exports = config => {
   config.set({
     basePath: '',
     frameworks: ['mocha', 'chai'],
-    files: ['client/tests/allTests.js'],
+    files: [
+      'client/tests/setup.js',
+      'client/tests/**/*.tests.js'
+    ],
     preprocessors: { ['client/tests/**/*.js']: ['webpack'] },
     webpack: require('./webpack.config'),
     webpackMiddleware: { stats: 'errors-only' },
