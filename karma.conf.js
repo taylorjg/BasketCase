7d42b404c67a48ceb84f84df7bb9cc0bce35f6bf
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-06-18 19:21:22 +0100

    Removed unnecessary stuff from karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 module.exports = config => {
   config.set({
-    basePath: '',
     frameworks: ['mocha', 'chai'],
     files: [
       'client/tests/setup.js',
@@ -9,13 +8,9 @@ module.exports = config => {
     preprocessors: { ['client/tests/**/*.js']: ['webpack'] },
     webpack: require('./webpack.config'),
     webpackMiddleware: { stats: 'errors-only' },
-    reporters: ['spec'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
     browsers: ['Chrome'],
-    singleRun: true,
-    concurrency: Infinity,
+    reporters: ['spec'],
+    autoWatch: false,
+    singleRun: true
   });
 };
